d42903c8a4d741fbfc9f950b2e58fdfd2c07599a
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2023-04-04 17:48:38 +0530

    Make sure only final text is in output

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(DEVICE)
 
 model = whisper.load_model("tiny.en", device=DEVICE)
-print(
-    f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
-    f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
-)
+# print(
+#     f"Model is {'multilingual' if model.is_multilingual else 'English-only'} "
+#     f"and has {sum(np.prod(p.shape) for p in model.parameters()):,} parameters."
+# )
 
 result = model.transcribe("audio.mp3")
 print(result["text"])
